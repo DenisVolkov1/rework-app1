9693b232d3950e648da0b91dc90edb7e0ceaaf00
user <user@DESKTOP-EMH7V6H>
2023-02-03 19:13:28 +0500

    кенр

--- src/main/java/main/util/SendBackupOnYandexDisk.java
+++ src/main/java/main/util/SendBackupOnYandexDisk.java
@@ -1,6 +1,8 @@
 package main.util;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,9 +13,18 @@ import java.sql.Types;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +34,7 @@ import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.model.ITemplateStart;
 
 @Component
 public class SendBackupOnYandexDisk {
@@ -35,24 +47,22 @@ public class SendBackupOnYandexDisk {
 	
 	@Scheduled(fixedDelayString = "${save_backup_yandex_disk.delay_milliseconds}")
 	public void send() {
-		
 		try {
-
+			int y = 1/0;
+			//PROPERTIEs
+			String pathOnBackupFolder = env.getProperty("save_backup_yandex_disk.path_MSSQLBackup");
+			String tokenOAuth = env.getProperty("save_backup_yandex_disk.tokenOAuth");
+			String toPathDiskYandex = env.getProperty("save_backup_yandex_disk.topath_disk_yandex");
 			//call proc CREATE_BACKUP_PROC
 			/////////////////////////////////////////////////////////
 			SimpleJdbcCall spCall = new SimpleJdbcCall(jdbcTemplate);
 			spCall.withProcedureName("CREATE_BACKUP_PROC");
 			spCall.addDeclaredParameter(new SqlParameter("path_MSSQLBackup", Types.NVARCHAR));
-			spCall.execute(env.getProperty("save_backup_yandex_disk.path_MSSQLBackup"));
+			spCall.execute(pathOnBackupFolder);
 			
-			System.out.println("Date : "+new Date().toLocaleString());
+			File backup = lastedBackup(pathOnBackupFolder);
 	
-		/*https://oauth.yandex.ru/authorize?response_type=token&client_id=dee1bd42bd86406eaf429a53afdcff62*/
-		
-			String pathFile = 
-					"C:\\Users\\Denis\\Documents\\Деталь.m3d";
-			String tokenOAuth = "y0_AgAEA7qjGxM3AAkPgwAAAADa5Qg6OREkZyy-QLaZpAivr57x4ERnQZk";
-			String toPathDiskYandex = "%2FDrawing1.dwg";
+			/*https://oauth.yandex.ru/authorize?response_type=token&client_id=dee1bd42bd86406eaf429a53afdcff62*/
 			
 			HttpRequest request;
 			HttpResponse<String> response;
@@ -83,7 +93,7 @@ public class SendBackupOnYandexDisk {
 			 request = HttpRequest.newBuilder()
 					  .uri(new URI((String) json.get("href")))
 					  .PUT(HttpRequest.BodyPublishers.ofFile(
-					    Paths.get(pathFile)))
+							  backup.toPath()))
 					  .build();
 
 			 response = HttpClient.newBuilder()
@@ -94,13 +104,64 @@ public class SendBackupOnYandexDisk {
 			 System.out.println(response.statusCode()); 
 			 System.out.println(response.body());
 		} catch (Exception e) {
-			e.printStackTrace();
+			// stack trace on mail 
+			sendMailError(e);	
+			
 		}	        
 	}
 	
+	private void sendMailError(Exception err) {
+		
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		err.printStackTrace(pw);
+		//PROPERTIEs
+		final String username = env.getProperty("save_backup_yandex_disk.gmail_smtp_mail");
+        final String password = env.getProperty("save_backup_yandex_disk.gmail_smtp_mail_app_password");
+        final String gmail_from_ardess = env.getProperty("save_backup_yandex_disk.gmail_smtp_mail_from_adress");
+        final String gmail_to_ardess = env.getProperty("save_backup_yandex_disk.gmail_smtp_mail_to_adress");
+        
+
+        Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage( session );
+            message.setFrom(new InternetAddress( gmail_from_ardess ));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse( gmail_to_ardess )
+            );
+            message.setSubject("ERROR!!! create backup database(REWORKBASE)! ");
+            message.setText(sw.toString());
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+		
+	}
+	private static List<File> listSortedFiles(String dir) {
+	    //return Stream.of(new File(dir).listFiles()).filter((t)->{return t.getAbsolutePath().matches(".*REWORKBASE_.*\\.bak$");}).sorted(Comparator.comparing(File::lastModified)).collect(Collectors.toList());
+		return Stream.of(new File(dir).listFiles()).filter((t)->{return t.getAbsolutePath().matches(".*\\.bak$");}).sorted(Comparator.comparing(File::lastModified)).collect(Collectors.toList());
+	}
 	
-	public static List<File> listSortedFiles(String dir) {
-	    return Stream.of(new File(dir).listFiles()).filter((t)->{return t.getAbsolutePath().matches(".*\\.bak$");}).sorted(Comparator.comparing(File::lastModified)).collect(Collectors.toList());
-		//return Stream.of(new File(dir).listFiles()).sorted(Comparator.comparing(File::lastModified)).collect(Collectors.toList());
+	private static File lastedBackup(String dir) {
+		List<File> backupFiles = listSortedFiles(dir);
+		return backupFiles.get(backupFiles.size()-1);
 	}
 }
