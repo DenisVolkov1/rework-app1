02c01a2c2805861e9c057a3a4a85a71673006e77
Denis <volkov.denis.mt190606@gmail.com>
2023-02-14 00:48:30 +0500

    498498498

--- src/main/java/main/config/MySpringConfig.java
+++ src/main/java/main/config/MySpringConfig.java
@@ -6,14 +6,15 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -62,12 +63,14 @@ public class MySpringConfig implements WebMvcConfigurer {
 
 	@Autowired
     public MySpringConfig(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;	
+        this.applicationContext = applicationContext;
     }
 			
 	@PostConstruct
 	public void sendMessagesAboutRunApp() throws MessagingException {
-		sm.sendMail("Run");
+		if(env.getProperty("rework_app1_monetka_runtype").equals("production")) {
+			sm.sendMail("--- Server Run !!! ---");
+		}
 	}
 	
     @Bean
--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -8,7 +8,7 @@
 
 $(function() {
 	
-		$('#d').datepicker({
+		$('#datepicker').datepicker({
 			format: 'dd.mm.yyyy',
 			locale: 'ru-ru',
             uiLibrary: 'bootstrap4'
